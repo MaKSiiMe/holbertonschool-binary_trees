e4da4588d2b5b5a7d7c31c7ad824782993bae5ee
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-12-09 11:52:34 +0000

    task 1 3 5 7 9 11 13

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -45,5 +50,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+void binary_tree_print(const binary_tree_t *);
 
 #endif
